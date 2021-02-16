384bcae27739fed7fb1ae238ffdc43930b0095be
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-16 20:01:10 +0530

    :sparkles: Create function for update category

--- routes/categories.js
+++ routes/categories.js
@@ -35,6 +35,20 @@ router.post('/', async (req, res) => {
     res.send(category);
 })
 
+router.put('/:id',  async (req, res) => {
+    const category = await Category.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
+    }, { 
+        new: true 
+    })
+
+    if (!category)
+        return res.status(404).send('Category cannot be created')
+    res.send(category);
+})
+
 router.delete('/:id', (req, res)=>{
     Category.findByIdAndRemove(req.params.id).then(category => {
         if(category){
