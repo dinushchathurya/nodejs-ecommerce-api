aedff2906bc503266a04dc773821750563bc8975
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-16 18:17:17 +0530

    :hammer: Add all the entities to product model

--- models/product.js
+++ models/product.js
@@ -1,11 +1,59 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    rischDescription: {
+        type: String,
+        default: ''
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    images: [{
+        type: String
+    }],
+    brand: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Number,
+        default: '0'
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true,
+    },
     countInStock: {
         type: Number,
-        required: true
+        required: true,
+        min: 0,
+        max: 255
+    },
+    rating: {
+        type: String,
+        default: 0
+    },
+    numReviews: {
+        type: Number,
+        default: 0,
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
     }
 })
 
