a3e1cb79c7179a6079e69450cfbd42d1cf9d54ca
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-16 20:26:36 +0530

    :bug: Fix typo in product model

--- models/product.js
+++ models/product.js
@@ -9,7 +9,7 @@ const productSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    rischDescription: {
+    richDescription: {
         type: String,
         default: ''
     },
