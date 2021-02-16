6587298ab1163b49014bd7a49b616b435984b36a
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-16 20:44:20 +0530

    :hammer: Add select method to fetch only name and image from api endpoint

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,7 @@ const Product = require('../models/product');
 const Category = require('../models/category');
 
 router.get('/', async (req, res)=> {
-    const productList = await Product.find();
+    const productList = await Product.find().select('name image -_id');
     if (!productList) {
         res.status(500), json({success:false})
     }
