a5505e567d21cf88181939df783e720894c81d60
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-16 18:48:49 +0530

    :sparkles: Create function to delete category

--- routes/categories.js
+++ routes/categories.js
@@ -26,4 +26,16 @@ router.post('/', async (req, res) => {
     res.send(category);
 })
 
+router.delete('/:id', (req, res)=>{
+    Category.findByIdAndRemove(req.params.id).then(category => {
+        if(category){
+            return res.status(200).json({ success: true, message: 'Category deleted successfully'})
+        } else {
+            return res.status(404).json({ success: false, message: 'Category cannot find'})
+        }
+    }).catch (err=>{
+        return res.status(400).json({ success: false, error: err})
+    })
+})
+
 module.exports = router;
