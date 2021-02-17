8bf4fea42cb228fb3044690a6e44565fc7096656
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-17 22:30:17 +0530

    :sparkles: Create user create endpoint

---
+++ routes/users.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/User');
+
+router.get('/', async (req, res) =>{
+    const userList = await User.find();
+
+    if(!userList) {
+        res.status(500).json({success:false})
+    }
+    res.send(userList);
+})
+
+router.post('/', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: req.body.passwordHash,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country
+    })
+
+    user = await user.save();
+
+    if (!user)
+        return res.status(404).send('User cannot be created')
+    res.send(user);
+})
+
+module.exports = router;
