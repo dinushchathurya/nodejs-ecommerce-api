8c1b4a4588a6f9a350b7e687df3d3b136ee48011
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-17 22:17:28 +0530

    :sparkles: Create user model

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    street: {
+        type: String,
+        default: ''
+    },
+    apartment: {
+        type: String,
+        default: ''
+    },
+    zip: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
