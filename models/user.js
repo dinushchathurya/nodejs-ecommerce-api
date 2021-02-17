708f092d1e57a50e7d8ee250e554ded33d9be8e1
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-17 22:18:42 +0530

    :hammer: Create virtual id instead of default ID

--- models/user.js
+++ models/user.js
@@ -43,4 +43,12 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+userSchema.set('toJSON', {
+    virtuals: true,
+});
+
 module.exports = mongoose.model('User', userSchema);
