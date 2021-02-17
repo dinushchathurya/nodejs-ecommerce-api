cf8bcd8ad23037ac67f56a8fed164fef4c9ad269
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-17 17:36:24 +0530

    :sparkles: Create product update function

--- routes/products.js
+++ routes/products.js
@@ -49,4 +49,31 @@ router.post('/', async (req, res) => {
     res.send(product);
 })
 
+router.put('/:id', async (req, res) => {
+
+    const category = await Category.findById(req.body.category);
+    if (!category)
+        return res.status(400).send('Invalid Category')
+
+    const product = await Product.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured
+    }, {
+        new: true
+    })
+
+    if (!product)
+        return res.status(500).send('Product cannot be updated')
+    res.send(product);
+})
+
 module.exports = router;
