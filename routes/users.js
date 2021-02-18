88f864bf6dad8e7cd4c0136f54b1bdc558384acb
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-18 21:40:48 +0530

    :sparkles: Pass isAdmin to token

--- routes/users.js
+++ routes/users.js
@@ -55,7 +55,8 @@ router.post('/login', async (req, res) => {
 
     if(user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
         const token = jwt.sign({
-            userID: user.id
+            userID: user.id,
+            isAdmin : user.isAdmin
         }, secret, {expiresIn : '1d'} )
         res.status(200).send({user: user.email, token: token});
     } else {
