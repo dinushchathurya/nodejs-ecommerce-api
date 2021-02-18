1c1ee06442c810664dcd099780591f74ce9c2f07
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-18 21:21:21 +0530

    :hammer: Add Regex to exclude all the get methods in products

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,13 +2,15 @@ const jwt  = require('express-jwt');
 
 function authJwt() {
     const secret = process.env.secret;
+    const api = process.env.API_URL;
     return jwt({
         secret,
         algorithms: ['HS256']
     }).unless({
         path: [
-           '/api/v1/users/login',
-           '/api/v1/users/register'
+            {url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
+            `${api}/users/login`,
+            `${api}/users/register`
         ]
     })
 }
