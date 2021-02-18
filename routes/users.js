bf05cbda5420a5a15acda4e213b5ddde024aac12
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-18 13:23:42 +0530

    :sparkles: Peering jwt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const User = require('../models/User');
