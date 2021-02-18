3c74f0c9dc89a58d9d09a93b451d74297b513a07
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-18 22:29:13 +0530

    :hammer: Revoke routes according to user role

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -5,7 +5,8 @@ function authJwt() {
     const api = process.env.API_URL;
     return jwt({
         secret,
-        algorithms: ['HS256']
+        algorithms: ['HS256'],
+        isRevoked : isRevoked
     }).unless({
         path: [
             {url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
@@ -14,6 +15,14 @@ function authJwt() {
             `${api}/users/register`
         ]
     })
+}
+
+async function isRevoked (req, payload, done){
+    if(!payload.isAdmin){
+        done(null, true)
+    } 
+
+    done();
 }
 
 module.exports = authJwt;
