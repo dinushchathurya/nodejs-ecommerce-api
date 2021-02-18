ee738418bcd4f546153ef127998eedad87f432ca
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-18 21:39:26 +0530

    :hammer: Exclude GET methods category routes

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -9,6 +9,7 @@ function authJwt() {
     }).unless({
         path: [
             {url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
+            {url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS']},
             `${api}/users/login`,
             `${api}/users/register`
         ]
