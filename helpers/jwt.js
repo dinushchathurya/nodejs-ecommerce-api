b3ebb07dbd8e9be422662ffcb8ff7679414dfc0b
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-19 22:26:06 +0530

    :hammer: Exclude public folder form authentication

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -9,6 +9,7 @@ function authJwt() {
         isRevoked : isRevoked
     }).unless({
         path: [
+            {url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
             {url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS']},
             `${api}/users/login`,
