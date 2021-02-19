7e279d1b7f75718b990b2ec19eb6b614ede0669d
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-19 08:48:44 +0530

    :sparkles: Create orderItem model schema

---
+++ models/order-item.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const orderItemSchema = mongoose.Schema({
+    quantity: {
+        type: Number,
+        required: true
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }
+})
+
+module.exports = mongoose.model('OrderItem', orderItemSchema);
