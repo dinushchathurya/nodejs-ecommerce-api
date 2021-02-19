7f4ad99ea2038a3b9a759619011018eb7ce92463
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-19 08:49:18 +0530

    :sparkles: Create orders model schema

---
+++ models/order.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+    orderItems : [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required: true,
+    }],
+    shippingAddress1: {
+        type: String
+    },
+    shippingAddress2: {
+        type: String
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    zip: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Pending'
+    },
+    totalPrices: {
+        type: Number
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    dateOrdered: {
+        type: Date,
+        dafault: Date.now
+    }
+})
+
+
+orderSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+orderSchema.set('toJSON', {
+    virtuals: true,
+});
+
+module.exports = mongoose.model('Order', orderSchema);
