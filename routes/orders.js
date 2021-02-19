cf47504d339a67ad82e2323f981c0a1de13e7c34
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-19 13:45:10 +0530

    :sparkles: Create methods for getting single order and make new order

---
+++ routes/orders.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express();
+
+const Order = require('../models/order');
+const OrderItem = require('../models/order-item');
+
+router.get('/', async (req, res) => {
+    const orderList = await Order.find().populate('name' ,'user').sort({'dateOrdered':-1});
+
+    if (!orderList) {
+        res.status(500).json({ success: false })
+    }
+    res.send(orderList)
+})
+
+router.get('/:id', async (req, res) => {
+    const order = await Order.findById(req.params.id).populate('name', 'user');
+
+    if (!order) {
+        res.status(500).json({ success: false })
+    }
+    res.send(order)
+})
+
+router.post('/', async (req, res) => {
+    const orderItemsIds = Promise.all(req.body.orderItems.map(async orderItem => {
+        let newOrderItem = new OrderItem({
+            quantity: orderItem.quantity,
+            product: orderItem.product
+        })
+
+        newOrderItem = await newOrderItem.save();
+
+        return newOrderItem._id;
+    }))
+
+    const orderItemsIdsResolved = await orderItemsIds;
+   
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user,
+    })
+
+    order = await order.save();
+
+    if (!order)
+        return res.status(404).send('Order cannot be created')
+    res.send(order);
+})
+
+module.exports = router;
