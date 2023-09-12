47be25825c7bd981145c2bd3f87ba7d2b5bab7a3
Dinush Chathurya <dinushchathurya21@gmail.com>
2023-09-12 21:34:46 +0100

    refactor: Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url:'mongodb+srv://admin:admin321@learning.eauwn.mongodb.net/ecommerce-api?retryWrites=true&w=majority'
+    url:'mongodb+srv://password:@learning.eauwn.mongodb.net/ecommerce-api?retryWrites=true&w=majority'
 }
